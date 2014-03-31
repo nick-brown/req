bf2f05a7c3db23d9c7ca75de1f3c992108fdb174
Nick Brown <nick@nickbrown.me>
2014-03-31 01:03:08 -0700

    added User Model

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 define(['marionette'], function(Marionette) {
     'use strict';
     
-    var app = new Marionette.Application();
+    var App = new Marionette.Application();
 
-    return app;
+    return App;
 });
